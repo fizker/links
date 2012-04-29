f44eca339933686d25b0999f2a64a49ec5b80567
Benjamin Horsleben <benjamin@fizkerinc.dk>
2012-04-29 21:25:32 +0200

    Some updates to link-routes and views.

--- src/routes/links.js
+++ src/routes/links.js
@@ -2,22 +2,49 @@
 
 module.exports = setupRoutes;
 
-var links = {}
+var links
 
 function setupRoutes(http) {
+	setupRoutes.links = links = {};
 	http.get('/links', allLinks);
 
-	http.get('/links/:url', getLink);
-	http.put('/links/:url', putLink);
-	http.delete('/links/:url', deleteLink);
+	http.post('/links', validateLink, postLink);
+	http.get('/links/new', newLink);
+	http.get('/links/:url', validateLink, getLink);
+	http.put('/links/:url', validateLink, putLink);
+	http.del('/links/:url', deleteLink);
 };
+function validateLink(request, response, next) {
+	var url = request.params && request.params.url
+	  , link = request.body
+	  , error
 
+	if(!link) {
+		error = 'No link given';
+		response.render('400', { status: 400, error: error });
+		next(new Error(error));
+		return;
+	}
+	if(url !== undefined) {
+		if(url !== link.url) {
+			error = 'URL does not match';
+			response.render('400', { status: 400, error: error });
+			next(new Error(error));
+			return;
+		}
+	}
+	next();
+};
+function newLink(request, response) {
+	response.render('link.edit.mustache');
+};
 function allLinks(request, response) {
 	response.render('links', Object.keys(links)
 		.map(function(key) {
 			return links[key];
 		}));
 };
+
 function getLink(request, response) {
 	var url = request.params.url
 	  , link = links[url]
@@ -28,6 +55,15 @@ function getLink(request, response) {
 	}
 
 	response.render('link', link);
+};
+
+function postLink(request, response) {
+	var link = request.body
+	  , url = link.url
+
+	links[url] = link;
+	response.local('message', 'link created');
+	allLinks(request, response);
 };
 function putLink(request, response) {
 	var url = request.params.url
