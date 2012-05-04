208ec1a149d8a0c735ea094d901f70e20b36290b
Benjamin Horsleben <benjamin@fizkerinc.dk>
2012-05-04 19:42:28 +0200

    Added unit tests for link-storage.

--- src/storage/links.js
+++ src/storage/links.js
@@ -24,17 +24,24 @@ function delLink(url, callback) {
 		url: url
 	};
 	db.collection('links', function(err, collection) {
-		collection.findAndModify(query, {}, null, { remove: true }, callback);
+		collection.remove(query, callback);
 	});
 };
 function getLink(url, callback) {
 	if(!callback) {
 		callback = url;
 		url = null;
-	}
 
+	}
 	db.collection('links', function(err, collection) {
 		if(err) return callback(err);
+
+		if(url) {
+			var query = { url: url }
+			collection.findOne(query, callback);
+			return;
+		}
+
 		collection.find().toArray(callback);
 	});
 }
---
+++ test/storage.links.js
@@ -0,0 +1,96 @@
+describe('storage.links.js', function() {
+	'use strict';
+
+	var factory = require('../src/storage/links')
+	  , db
+	  , storage
+	  , callback
+	  , linkCollection
+
+	beforeEach(function() {
+		callback = sinon.spy();
+		linkCollection =
+			{find: sinon.stub()
+			,findOne: sinon.stub()
+			,remove: sinon.stub()
+			,save: sinon.stub()
+			};
+		db = {
+			collection: sinon.stub().withArgs('links').yields(null, linkCollection)
+		};
+		storage = factory(db);
+	});
+	describe('When calling add(link)', function() {
+		beforeEach(function() {
+			linkCollection.save
+				.withArgs({ url: 'abc', text:'def' })
+				.yields(null, { new: 'object' });
+			storage.add({ url: 'abc', text:'def' }, callback);
+		});
+		it('should call collection.save', function() {
+			expect(linkCollection.save)
+				.to.have.been.calledWith({ url:'abc', text:'def' });
+		});
+		it('should call callback with the saved object', function() {
+			expect(callback)
+				.to.have.been.calledWith(null, { new: 'object' });
+		});
+	});
+	describe('When calling del(url)', function() {
+		beforeEach(function() {
+			linkCollection.remove
+				.withArgs({ url: 'abc' })
+				.yields(null, { url:'abc', text:'def' });
+			storage.del('abc', callback);
+		});
+		it('should ask the collection to remove the specified link', function() {
+			expect(linkCollection.remove)
+				.to.have.been.calledWith({ url: 'abc' });
+		});
+		it('should call callback with the removed object', function() {
+			expect(callback)
+				.to.have.been.calledWith(null, { url:'abc', text:'def' });
+		});
+	});
+	describe('When calling get(url)', function() {
+		var findResults
+
+		beforeEach(function() {
+			linkCollection.findOne
+				.withArgs({ url: 'abc' })
+				.yields(null, { url: 'abc', text: 'def' });
+
+			storage.get('abc', callback);
+		});
+		it('should ask for links-collection', function() {
+			expect(db.collection).to.have.been.calledWith('links');
+		});
+		it('should hand the found object to the callback', function() {
+			expect(callback)
+				.to.have.been.calledWith(null, { url:'abc', text:'def' });
+		});
+	});
+	describe('When calling get()', function() {
+		var findResults
+
+		beforeEach(function() {
+			findResults = {
+				toArray: sinon.stub()
+			};
+			findResults.toArray.yields(null, [{url: 'a'}, {url: 'b'}]);
+
+			linkCollection.find
+				.withArgs()
+				.returns(findResults);
+
+			storage.get(callback);
+		});
+		it('should ask for link-collection', function() {
+			expect(db.collection).to.have.been.calledWith('links');
+		});
+		it('should call callback with the returned objects', function() {
+			expect(callback)
+				.to.have.been.calledWith(null, [{url:'a'}, {url:'b'}]);
+		});
+	});
+});
