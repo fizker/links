5d56dbfb1290cbddce3b494e7ddd3f68799bb255
Benjamin Horsleben <benjamin@fizkerinc.dk>
2012-05-14 04:43:17 +0200

    Added tests + impl for http auth.

---
+++ src/server/middleware/auth.js
@@ -0,0 +1,41 @@
+'use strict';
+
+module.exports = auth
+
+function auth(request, response, next) {
+	if(handleHttpAuth(request, userHasAuthed)) {
+		return;
+	}
+	// check other means of authing here (token, cookie, whatever).
+
+	userHasAuthed(null, false);
+
+	function userHasAuthed(err, result) {
+		if(result) {
+			return next();
+		}
+		response.render('errors/401', { status: 401 });
+	};
+}
+
+function handleHttpAuth(request, next) {
+	var authHeader = request.header('Authorization')
+	  , base64
+	  , debase64
+	  , split
+	  , username
+	  , password
+
+	if(!authHeader) {
+		return false;
+	}
+
+	base64 = authHeader.replace('Basic ', '')
+	debase64 = new Buffer(base64, 'base64').toString('ascii');
+	split = debase64.split(/:/);
+	username = split[0];
+	password = split[1];
+
+	request.storage.users.verify(username, password, next);
+	return true;
+};
---
+++ test/middleware.auth.js
@@ -0,0 +1,69 @@
+describe('middleware.auth.js', function() {
+	var middleware = require('../src/server/middleware/auth')
+	  , request
+	  , response
+	  , nextSpy
+
+	beforeEach(function() {
+		request = {
+			header: sinon.stub(),
+			storage: {
+				users: {
+					verify: sinon.stub()
+				}
+			}
+		};
+		request.storage.users.verify.yields(null, false);
+		request.storage.users.verify.withArgs('valid', 'creds').yields(null, true);
+
+		response = {
+			send: sinon.stub(),
+			render: sinon.stub()
+		};
+		nextSpy = sinon.spy();
+	});
+	describe('When authorized', function() {
+		describe('with valid credentials', function() {
+			beforeEach(function() {
+				var credentials = 'valid:creds'
+					// created with btoa('creds:creds')
+				  , asBase64 = 'dmFsaWQ6Y3JlZHM='
+
+				request.header.withArgs('Authorization').returns('Basic dmFsaWQ6Y3JlZHM=');
+				middleware(request, response, nextSpy);
+			});
+			it('should call next', function() {
+				expect(nextSpy).to.have.been.calledWithExactly();
+			});
+		});
+		describe('with invalid credentials', function() {
+			beforeEach(function() {
+				var credentials = 'invalid:creds'
+					// created with btoa('abc:def')
+				  , asBase64 = 'aW52YWxpZDpjcmVkcw=='
+
+				request.header.withArgs('Authorization').returns('Basic aW52YWxpZDpjcmVkcw==');
+				middleware(request, response, nextSpy);
+			});
+			it('should send http status 401', function() {
+				var status = response.render.lastCall.args[1].status;
+				expect(status).to.be.eql(401);
+			});
+		});
+	});
+	describe('When not authorized', function() {
+		beforeEach(function() {
+			middleware(request, response, nextSpy);
+		});
+		it('should send http status 401', function() {
+			var status = response.render.lastCall.args[1].status;
+			expect(status).to.be.eql(401);
+		});
+		it('should request the 401 error page', function() {
+			var view = response.render.lastCall.args[0];
+			expect(view)
+				.to.have.string('401')
+				.and.to.have.string('errors');
+		});
+	});
+});
